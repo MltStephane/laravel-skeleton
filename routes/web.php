ba8230b0172c92358a2a9970c695c5e38ee86c37
Stéphane MULOT <stephane@mulot.dev>
2023-03-25 19:10:40 +0100

    feat: implements logout route with get method

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::impersonate();
+
+Route::get('logout', [AuthenticatedSessionController::class, 'destroy'])->name('get-logout');
 
 Route::name('public.')->group(function () {
     Route::get('/', \App\Http\Livewire\Public\Homepage::class)->name('homepage');
